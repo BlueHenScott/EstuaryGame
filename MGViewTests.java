17b9ee4a437e4a98b52a5cd5b570d511961e810e
Elena <elenasmacd@gmail.com>
2018-04-21 18:57:43 -0400

    frame for tests

---
+++ MGViewTests.java
@@ -0,0 +1,15 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MGViewTests {
+	
+	/*
+	MGView tester;
+		
+	public static void testSetup() {
+		tester = new MGView();
+		}
+		
+	public void test viewMethod() {}
+	*/
+	
+}
