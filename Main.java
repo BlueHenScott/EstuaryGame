6ac4986ed346755c974f6a8134af7920597942ba
sophiafreaney <sfreaney@udel.edu>
2018-04-21 19:13:23 -0400

    stuff again

--- Main.java
+++ Main.java
@@ -3,11 +3,7 @@ import javax.swing.JPanel;
 @SuppressWarnings("serial")
 public class Main extends JPanel {
 	public static void main(String[] args) {
-		System.out.println("Testing main");
-	}
-}
-class TitleScreen {
-	public TitleScreen() {
-		System.out.println("Testing title screen");
+		// Create and start a new Controller
+		TitleController con = new TitleController();
 	}
 }
