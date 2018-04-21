ce947b2adb955f71a61635ac7bc28bb080585fc9
sophiafreaney <sfreaney@udel.edu>
2018-04-21 18:32:21 -0400

    committing main

---
+++ Main.java
@@ -0,0 +1,13 @@
+import javax.swing.JPanel;
+
+@SuppressWarnings("serial")
+public class Main extends JPanel {
+	public static void main(String[] args) {
+		System.out.println("Testing main");
+	}
+}
+class TitleScreen {
+	public TitleScreen() {
+		System.out.println("Testing title screen");
+	}
+}
