9ceca041ed52912d2cda9034fed649af12c90dc7
sophiafreaney <sfreaney@udel.edu>
2018-04-21 21:09:54 -0400

    adding back titlemain

---
+++ TitleMain.java
@@ -0,0 +1,9 @@
+import javax.swing.JPanel;
+
+@SuppressWarnings("serial")
+public class TitleMain extends JPanel {
+	public static void main(String[] args) {
+		// Create and start a new Controller
+		TitleController con = new TitleController();
+	}
+}
