ecad240e1b217ce56744280a073c87733183f099
Scott <sbenton@udel.edu>
2018-04-22 21:52:14 -0400

    Added TitleScreen Graphic

--- TitleView.java
+++ TitleView.java
@@ -1,6 +1,5 @@
 import java.awt.BorderLayout;
 import java.awt.Color;
-import java.awt.FlowLayout;
 import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.GridBagLayout;
@@ -23,63 +22,56 @@ import java.awt.event.ActionListener;
 public class TitleView extends JPanel{
 	
 	// Constants for the size of the view
-	private int viewWidth = 1500;
-	private int viewHeight = 900;
+	private final int viewWidth = 1500;
+	private final int viewHeight = 900;
 	
+
+	private BufferedImage background;
 	
 	private JFrame frame;
 	private JButton startButton = new JButton("Start");
 	
-	// Background image
-	private BufferedImage background;
-	
 	public TitleView(JFrame f) {
-		frame = f;
+	
 		
-		// Player net image;
-		String backgroundLoc = "images/titlescreen.png";
-		background = createImage(backgroundLoc);
 		startButton.setFont(new Font("Arial", Font.PLAIN, 40));
 		
-		JPanel buttonPanel = new JPanel(new GridLayout());
-		buttonPanel.setBackground(Color.BLUE);
-	    buttonPanel.add(startButton);
-	    
+		JPanel buttonPanel = new JPanel(new GridLayout(1,2,4,4));
+	    buttonPanel.add(startButton);	
+
+		String backgroundLoc = "images/titlescreen.png";
+		background = createImage(backgroundLoc);
+
+		
+
 	    JPanel imagePanel = new JPanel (new GridLayout());
-	    JLabel picLabel = new JLabel(new ImageIcon(background.getScaledInstance(viewWidth, viewHeight-175, Image.SCALE_SMOOTH)));
+	    JLabel picLabel = new JLabel(new ImageIcon(background.getScaledInstance(viewWidth, viewHeight, Image.SCALE_SMOOTH)));
 	    imagePanel.add(picLabel);
-		// Add this game to the jFrame and focus on it
-	    FlowLayout experimentLayout = new FlowLayout();
-	    frame.setLayout(experimentLayout);
+		
+		frame = f;
+		frame.getContentPane().removeAll();
+		
+		frame.getContentPane().setBackground(Color.CYAN);
+		frame.getContentPane().add(buttonPanel, BorderLayout.PAGE_END);
 		frame.getContentPane().add(imagePanel);
-		frame.getContentPane().add(buttonPanel);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setSize(viewWidth, viewHeight);
+   		frame.setSize(viewWidth, viewHeight);
 		frame.setVisible(true);
 		frame.setFocusable(true);
-		// Set focus on the panel
-		this.requestFocus();
-		frame.repaint();
 	}
-	protected void paintComponent(Graphics g) {
-		// Draw the background image
-		g.drawImage(background, 0, 0, null, this);
-	}
-	
     // Action Listener for Start/Stop Button
     public void addStartListener(ActionListener start) {
     	startButton.addActionListener(start);
     }
-	// Reads in images
-	private BufferedImage createImage(String loc) {
-		BufferedImage bufferedImage;
-		try {
-			bufferedImage = ImageIO.read(new File(loc));
-			return bufferedImage;
-		} catch(IOException e) {
-			e.printStackTrace();
-		}
-		return null;
-	}
+ // Reads in images
+ 	private BufferedImage createImage(String loc) {
+ 		BufferedImage bufferedImage;
+ 		try {
+ 			bufferedImage = ImageIO.read(new File(loc));
+ 			return bufferedImage;
+ 		} catch(IOException e) {
+ 			e.printStackTrace();
+ 		}
+ 		return null;
+ 	}
 }
-
